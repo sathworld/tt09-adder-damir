8f14213975d1976111b0bdfb2c1b66bdd11e285f
Damir G <48067596+sathworld@users.noreply.github.com>
2024-09-18 15:11:18 -0400

    Update project.v
    Testing conditional inverting of a signal

--- src/project.v
+++ src/project.v
@@ -17,11 +17,12 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign ui_in_inverted = rst_n? ~ui_in : ui_in;
+  assign uo_out  = ui_in_inverted + uio_in + rst_n;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, 1'b0};
 
 endmodule
