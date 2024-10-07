a8617caaf181730da71a3a8b9fa0ed15b2e24e9f
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-10-07 15:57:22 -0400

    reduction operator not working... let me try to test existing code first

--- src/project.v
+++ src/project.v
@@ -107,6 +107,6 @@ module onebitfa (a, b, cin, sum, cout);
     output wire sum; // Sum
     output wire cout; // Carry out
 
-    assign sum = ^(a, b, cin); // Take the XOR with fan in of 3 of a, b and cin and put the result in sum
-    assign cout = |((a & b), (cin & a), (cin & b)); // Take the OR with fan in of 3 of a&b, cin&a and cin&b and put the result in cout
+    xor xor1(sum, a, b, cin); // Take the XOR with fan in of 3 of a, b and cin and put the result in sum
+    or or1(cout, (a & b), (cin & a), (cin & b)); // Take the OR with fan in of 3 of a&b, cin&a and cin&b and put the result in cout
 endmodule
