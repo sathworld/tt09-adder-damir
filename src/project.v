a9e4c4e1f1f85088213e1e87400f3b093afdaf7c
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-10-07 15:25:50 -0400

    Updated the modules to use reduction operators, commented the test project out

--- src/project.v
+++ src/project.v
@@ -5,26 +5,26 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
+// module tt_um_example (
+//     input  wire [7:0] ui_in,    // Dedicated inputs
+//     output wire [7:0] uo_out,   // Dedicated outputs
+//     input  wire [7:0] uio_in,   // IOs: Input path
+//     output wire [7:0] uio_out,  // IOs: Output path
+//     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+//     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+//     input  wire       clk,      // clock
+//     input  wire       rst_n     // reset_n - low to reset
+// );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+//   // All output pins must be assigned. If not used, assign to 0.
+//   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+//   assign uio_out = 0;
+//   assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+//   // List all unused inputs to prevent warnings
+//   wire _unused = &{ena, clk, rst_n, 1'b0};
 
-endmodule
+// endmodule
 
 module accumulator (
     input  wire       clk, // Clock signal
@@ -62,8 +62,8 @@ module add_sub_8bit_sync (
   assign bus = enable_output ? sum : 8'bZZZZZZZZ; // Tri-state buffer to connect to the bus;
   always @(posedge clk ) begin
     if (enable_output)
-        CF <= carry_out;
-        ZF <= res_zero;
+      CF <= carry_out;
+      ZF <= res_zero;
   end
 endmodule
 
@@ -107,6 +107,6 @@ module onebitfa (a, b, cin, sum, cout);
     output wire sum; // Sum
     output wire cout; // Carry out
 
-    xor xor1(sum, a, b, cin); // Take the XOR with fan in of 3 of a, b and cin and put the result in sum
-    or or1(cout, (a & b), (cin & a), (cin & b)); // Take the OR with fan in of 3 of a&b, cin&a and cin&b and put the result in cout
+    assign sum = ^(a, b, cin); // Take the XOR with fan in of 3 of a, b and cin and put the result in sum
+    assign cout = |((a & b), (cin & a), (cin & b)); // Take the OR with fan in of 3 of a&b, cin&a and cin&b and put the result in cout
 endmodule
