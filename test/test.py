8048fea4fd64b074836541e17c1ba1a1d82cb861
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-10-07 15:54:24 -0400

    Wrote a tb to test loading into the register

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.types.logic import Logic
 
 @cocotb.test()
 async def test_project(dut):
@@ -16,25 +16,23 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.bus.value = Logic("Z")
+    dut.load.value = 0
+    dut.enable_output.value = 0
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    dut.load.value = 1
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.bus.value = 128
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.regA.value == 128
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
