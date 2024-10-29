f9b4ae788d5ca007ee8322ceb48a62be52bd1f8d
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-10-29 10:27:31 -0400

    updated project v to include the 2 modules

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_adder_accumulator_sathworld (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,11 +16,23 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire bus [7:0];
+  wire nLa;
+  wire Ea;
+  wire Eu;
+  wire regA [7:0];
+  wire regB [7:0];
+  wire sub;
+  wire CF;
+  wire ZF;
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  alu aluobj(clk, Eu, regA, regB, sub, bus, CF, ZF);
+  accumulator_register accumulatorobj(clk, bus, load, Ea, regA);
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
