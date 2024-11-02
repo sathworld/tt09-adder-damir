2102b929c9270e2c8ab8c859786e6e7bf4aecd66
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-01 22:17:26 -0400

    WHAT THE .

--- src/project.v
+++ src/project.v
@@ -29,8 +29,8 @@ module tt_um_adder_accumulator_sathworld (
   wire ZF;
   
   
-  // assign bus = ui_in; // Input path
-  // assign uo_out = uio_in[0] ? ui_in : regA; 
+  assign bus = ui_in; // Input path
+  assign uo_out = uio_in[0] ? ui_in : regA; 
   
   
   // // All output pins must be assigned. If not used, assign to 0.
@@ -41,34 +41,34 @@ module tt_um_adder_accumulator_sathworld (
   assign uio_out[4] = 0;
   assign uio_out[5] = 0;
 
-  // assign uio_oe[0] = 0;
-  // assign uio_oe[1] = 0;
-  // assign uio_oe[2] = 0;
-  // assign uio_oe[3] = 0;
-  // assign uio_oe[4] = 0;
-  // assign uio_oe[5] = 0;
-  // assign uio_oe[6] = 1;
-  // assign uio_oe[7] = 1;
+  assign uio_oe[0] = 0;
+  assign uio_oe[1] = 0;
+  assign uio_oe[2] = 0;
+  assign uio_oe[3] = 0;
+  assign uio_oe[4] = 0;
+  assign uio_oe[5] = 0;
+  assign uio_oe[6] = 1;
+  assign uio_oe[7] = 1;
 
-  // assign nLa = uio_in[1];
-  // assign nLb = uio_in[2];
-  // assign Ea = uio_in[3];
-  // assign Eb = 0;
-  // assign Eu = uio_in[4];
-  // assign sub = uio_in[5];
-  assign nLa = 1;
-  assign nLb = 1;
-  assign Ea = 0;
+  assign nLa = uio_in[1];
+  assign nLb = uio_in[2];
+  assign Ea = uio_in[3];
   assign Eb = 0;
-  assign Eu = 0;
-  assign sub = 0;
+  assign Eu = uio_in[4];
+  assign sub = uio_in[5];
+  // assign nLa = 1;
+  // assign nLb = 1;
+  // assign Ea = 0;
+  // assign Eb = 0;
+  // assign Eu = 0;
+  // assign sub = 0;
   assign uio_out[6] = CF;
   assign uio_out[7] = ZF;
 
 
   alu aluobj(clk, Eu, regA, regB, sub, bus, CF, ZF);
   accumulator_register accumulatorobj(clk, bus, nLa, Ea, regA);
-  accumulator_register breg(clk, bus, nLb, Eb, regB);
+//  accumulator_register breg(clk, bus, nLb, Eb, regB);
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, rst_n, uio_in[6], uio_in[7], 1'b0};
 
