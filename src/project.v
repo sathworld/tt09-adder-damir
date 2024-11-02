6dc85eef40a3e760cdce245b00b9789f03e8465f
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-02 16:39:54 -0400

    confusion

--- src/project.v
+++ src/project.v
@@ -35,46 +35,46 @@ module tt_um_adder_accumulator_sathworld (
   assign bus = (!Ea & !Eu) ? ui_in_buf : 8'bZZZZZZZZ;
   //assign bus = Ea ? busregA : (Eu ? busAdd : ui_in_buf);
   // assign bus = ena ? ui_in: 8'bZZZZZZZZ; // Input path
-  assign uo_out = uio_in[7] ? bus : regA; 
+  assign uo_out = uio_in[0] ? bus : regA; 
   
   
   // // All output pins must be assigned. If not used, assign to 0.
-  assign uio_out[7] = 0;
-  assign uio_out[6] = 0;
-  assign uio_out[5] = 0;
-  assign uio_out[4] = 0;
-  assign uio_out[3] = 0;
+  assign uio_out[0] = 0;
+  assign uio_out[1] = 0;
   assign uio_out[2] = 0;
+  assign uio_out[3] = 0;
+  assign uio_out[4] = 0;
+  assign uio_out[5] = 0;
 
-  assign uio_oe[7] = 0;
-  assign uio_oe[6] = 0;
-  assign uio_oe[5] = 0;
-  assign uio_oe[4] = 0;
-  assign uio_oe[3] = 0;
+  assign uio_oe[0] = 0;
+  assign uio_oe[1] = 0;
   assign uio_oe[2] = 0;
-  assign uio_oe[1] = 1;
-  assign uio_oe[0] = 1;
+  assign uio_oe[3] = 0;
+  assign uio_oe[4] = 0;
+  assign uio_oe[5] = 0;
+  assign uio_oe[6] = 1;
+  assign uio_oe[7] = 1;
 
-  assign nLa = uio_in[6];
-  assign nLb = uio_in[5];
-  assign Ea = uio_in[4];
+  assign nLa = uio_in[1];
+  assign nLb = uio_in[2];
+  assign Ea = uio_in[3];
   assign Eb = 0;
-  assign Eu = uio_in[3];
-  assign sub = uio_in[2];
+  assign Eu = uio_in[4];
+  assign sub = uio_in[5];
   // assign nLa = 1;
   // assign nLb = 1;
   // assign Ea = 0;
   // assign Eb = 0;
   // assign Eu = 0;
   // assign sub = 0;
-  assign uio_out[1] = CF;
-  assign uio_out[0] = ZF;
+  assign uio_out[6] = CF;
+  assign uio_out[7] = ZF;
 
 
   alu aluobj(clk, Eu, regA, regB, sub, bus, CF, ZF);
   accumulator_register accumulatorobj(clk, bus, nLa, Ea, regA);
   accumulator_register breg(clk, bus, nLb, Eb, regB);
   // List all unused inputs to prevent warnings
-  wire _unused = &{rst_n, ena, uio_in[6], uio_in[7], 1'b0};
+  wire _unused = &{rst_n, ena, uio_in[1], uio_in[0], uio_in[6], uio_in[7], 1'b0};
 
 endmodule
