f3332163148163012b7ed8074d13fc69eae90b89
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-03 15:03:03 -0500

    fuck this shit

--- src/project.v
+++ src/project.v
@@ -30,7 +30,7 @@ module tt_um_adder_accumulator_sathworld (
   wire bus_regA_sel;
   
   // ui_in NEEDS A BUFFER
-  always @(posedge clk or negedge Ea or negedge Eu) begin
+  always @(posedge clk) begin
     if (Ea | Eu)
       ui_in_buf <= 8'b00000000;
     else
