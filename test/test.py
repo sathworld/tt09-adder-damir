e1143f2d19a66fd0d8d7be948a792476d74923ed
Damir Gazizullin <dgazizul@uwaterloo.ca>
2024-11-03 11:50:31 -0500

    fixed gltest?

--- test/test.py
+++ test/test.py
@@ -53,11 +53,7 @@ def bus_values(dut):
     if (not GLTEST):
         dut._log.info(f"Current bus values: input={dut.ui_in.value}, bus={dut.user_project.bus.value}, output={dut.uo_out.value}")
     else:
-        bus = LogicArray("XXXXXXXX")
-        for i in range(8):
-            a = i
-            #bus[i] = dut.user_project."\accumulatorobj.bus"[i]
-        dut._log.info(f"Current bus values: input={dut.ui_in.value}, bus={bus}, output={dut.uo_out.value}")
+        dut._log.info(f"Current bus values: input={dut.ui_in.value}, output={dut.uo_out.value}")
 def control_signal_values(dut):
     vals = dut.uio_in.value
     dut._log.info(f"Current control signal: {dut.uio_in.value}")
@@ -116,10 +112,7 @@ async def init(dut):
     if (not GLTEST):
         assert (dut.uo_out.value == "zzzzzzzz") and (dut.user_project.bus.value == dut.uo_out.value), f"""Bus load failed: expected {LogicArray("ZZZZZZZZ")}, got bus={dut.user_project.bus.value}, output={dut.uo_out.value}"""
     else:
-        bus = LogicArray("XXXXXXXX")
-        for i in range(8):
-            bus[i] = dut.user_project.accumulatorobj.bus[i]
-        assert (dut.uo_out.value == "zzzzzzzz") and (bus == dut.uo_out.value), f"""Bus load failed: expected {LogicArray("ZZZZZZZZ")}, got bus={bus}, output={dut.uo_out.value}"""
+        assert (dut.uo_out.value == "zzzzzzzz"), f"""Bus load failed: expected {LogicArray("ZZZZZZZZ")}, got output={dut.uo_out.value}"""
 
 
 async def enable_regA_output(dut):
@@ -131,8 +124,13 @@ async def enable_regA_output(dut):
     dut._log.info("Wait for Hi-Z to propogate to bus, and for control signals to update (Falling edge)")
     await FallingEdge(dut.clk)
     control_signal_values(dut)
-    assert (read_control_signal_bit(dut.uio_in.value,3) == 1) and (dut.user_project.Ea.value == 1), "Ea did not get set"
-    assert (dut.uo_out.value != "zzzzzzzz") and (dut.uo_out.value != "xxxxxxxx") and (dut.user_project.regA.value == dut.uo_out.value), f"RegA read failed: got {dut.uo_out.value}"
+    if (not GLTEST):
+        assert (read_control_signal_bit(dut.uio_in.value,3) == 1) and (dut.user_project.Ea.value == 1), "Ea did not get set"
+        assert (dut.uo_out.value != "zzzzzzzz") and (dut.uo_out.value != "xxxxxxxx") and (dut.user_project.regA.value == dut.uo_out.value), f"RegA read failed: got {dut.uo_out.value}"
+    else:
+        assert (read_control_signal_bit(dut.uio_in.value,3) == 1), "Ea did not get set"
+        assert (dut.uo_out.value != "zzzzzzzz") and (dut.uo_out.value != "xxxxxxxx"), f"RegA read failed: got {dut.uo_out.value}"
+ 
 
 
 async def regAB_load_helper(dut, reg, val):
@@ -157,7 +155,10 @@ async def regAB_load_helper(dut, reg, val):
     await RisingEdge(dut.clk)
     bus_values(dut)
     control_signal_values(dut)
-    assert (dut.uo_out.value == val) and (dut.user_project.bus.value == val), f"Bus load failed: expected {val}, got {dut.uo_out.value}"
+    if (not GLTEST):
+        assert (dut.uo_out.value == val) and (dut.user_project.bus.value == val), f"Bus load failed: expected {val}, got {dut.uo_out.value}"
+    else:
+        assert (dut.uo_out.value == val), f"Bus load failed: expected {val}, got {dut.uo_out.value}"
     dut._log.info("Wait for val to be latched to the registers")
 
     dut._log.info("Reset loading signals")
@@ -179,7 +180,10 @@ async def accumulator_test_randint(dut):
     dut.uio_in.value = setbit(dut.uio_in.value, 0, 0)
     # dut.uio_in.value[0] = 0 # Output RegA
     await FallingEdge(dut.clk)
-    assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+    if (not GLTEST):
+        assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+    else:
+        assert (dut.uo_out.value == test_value), f"Accumulator output test: expected {test_value}, got output={dut.uo_out.value}"
     dut._log.info("Accumulator rand int module test completed successfully.")
 
 @cocotb.test()
@@ -188,17 +192,23 @@ async def accumulator_test_randint_out(dut):
     await init(dut)
     
     test_value = randint(0,255)
-    dut._log.info(f"Test load operation with val={test_value}")
+    dut._log.info(f"Test load operation with val={test_value}, bin={test_value:#010b}")
     await regAB_load_helper(dut, 'a', test_value)
     # Test enable output functionality
     await enable_regA_output(dut)
-    assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), "Enable output failed: bus did not reflect loaded accumulator value"
+    if (not GLTEST):
+        assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), "Enable output failed: bus did not reflect loaded accumulator value"
+    else:
+        assert (dut.uo_out.value == test_value), "Enable output failed: bus did not reflect loaded accumulator value"
     dut._log.info("Accumulator enable output successful")
     dut.uio_in.value = setbit(dut.uio_in.value, 0, 0)
     # dut.uio_in.value[0] = 0 # Output RegA
     await FallingEdge(dut.clk)
-    dut._log.info(dut.user_project.regA.value)
-    assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+    if (not GLTEST):
+        dut._log.info(dut.user_project.regA.value)
+        assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+    else:
+        assert (dut.uo_out.value == test_value), f"Accumulator output test: expected {test_value}, got output={dut.uo_out.value}"
     dut._log.info("Accumulator rand int output module test completed successfully.")
 
 @cocotb.test()
@@ -210,18 +220,24 @@ async def accumulator_test_shuffled_range(dut):
     shuffle(test_values)
     test_values = test_values[:25]
     for test_value in test_values:
-        dut._log.info(f"Test load operation with val={test_value}")
+        dut._log.info(f"Test load operation with val={test_value}, bin={test_value:#010b}")
         await regAB_load_helper(dut, 'a', test_value)
         # Test enable output functionality
         await enable_regA_output(dut)
-        assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), "Enable output failed: bus did not reflect loaded accumulator value"
+        if (not GLTEST):
+            assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), "Enable output failed: bus did not reflect loaded accumulator value"
+        else:
+            assert (dut.uo_out.value == test_value), "Enable output failed: bus did not reflect loaded accumulator value"
         dut._log.info("Accumulator enable output successful")
         controlsignal_value = setbit(dut.uio_in.value, 0, 0)
         dut.uio_in.value = setbit(controlsignal_value, 3, 0)
         # dut.uio_in.value[0] = 0 # Output RegA
         await RisingEdge(dut.clk)
-        dut._log.info(dut.user_project.regA.value)
-        assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+        if (not GLTEST):
+            dut._log.info(dut.user_project.regA.value)
+            assert (dut.uo_out.value == test_value) and (test_value == dut.user_project.regA.value), f"Accumulator output test: expected {test_value}, got regA={dut.user_project.regA.value} output={dut.uo_out.value}"
+        else:
+            assert (dut.uo_out.value == test_value), f"Accumulator output test: expected {test_value}, got output={dut.uo_out.value}"
         dut.uio_in.value = setbit(dut.uio_in.value, 0, 1)
         # dut.uio_in.value[0] = 1 # Output Bus
         await FallingEdge(dut.clk)
